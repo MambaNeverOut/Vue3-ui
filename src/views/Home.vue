d8566598d945ff135a2b499d695934fcda012efb
Irelia <yaokai729@outlook.com>
2022-02-28 22:35:38 +0800

    初始化项目，添加vue-router‘

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>ui</h1>
+      <h2>牛蛙牛蛙</h2>
+      <p class="actions">
+        <a href="">GitHub</a>
+        <a href="">开始</a>
+      </p>
+    </div>
+  </div>
+</template>
