846333b418c074d0ac1eb4bf4ac8a7b19605dde8
yaokai <yaokai729@outlook.com>
2022-03-15 10:34:02 +0800

    创建Markdown组件，代码优化技巧

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,11 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
+import Markdown from './components/Markdown.vue'
+import { h } from "vue";
+
+const md = (fileName) => h(Markdown, { path: `../markdown/${fileName}.md`, key: fileName })
+
 const history = createWebHashHistory();
 export const router = createRouter({
   history: history,
@@ -8,12 +13,11 @@ export const router = createRouter({
     { path: '/', component: Home },
     {
       path: '/doc', component: Doc,
-
       children: [
         { path: '', component: () => import("./components/DocDemo.vue") },
-        { path: 'intro', component: () => import("./views/Intro.vue") },
-        { path: 'install', component: () => import("./views/Install.vue") },
-        { path: 'get-started', component: () => import("./views/GetStarted.vue") },
+        { path: 'intro', component: md('intro') },
+        { path: 'install', component: md('install') },
+        { path: 'get-started', component: md('get-started') },
         { path: 'switch', component: () => import("./components/SwitchDemo.vue") },
         { path: 'button', component: () => import("./components/ButtonDemo.vue") },
         { path: 'dialog', component: () => import("./components/DialogDemo.vue") },
