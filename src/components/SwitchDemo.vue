47c22abc9a3b2b232e6bae5ea9f7f1fab705ff7c
yaokai <yaokai729@outlook.com>
2022-03-15 14:00:58 +0800

    添加文档中的代码示例

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,23 +1,66 @@
 <template>
   <div>
-    <Switch v-model:value="bool" />
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <Switch v-model:value="bool" />
+      </div>
+      <div class="demo-actions">
+        <!-- <Button @click="toggleCode">查看代码</Button> -->
+      </div>
+      <div class="demo-code">
+        <pre v-text="Switch1Demo.__sourceCode"></pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
+import Switch1Demo from "../code/Switch1.demo.vue";
+import Switch2Demo from "../code/Switch2.demo.vue";
+console.log(Switch1Demo.__sourceCode);
+
 export default {
   components: {
     Switch,
+    Switch1Demo,
+    Switch2Demo,
   },
   setup() {
     const bool = ref(false);
 
-    return { bool };
+    return { bool, Switch1Demo };
   },
 };
 </script>
 
-<style lang='' scoped>
+<style lang='scss' scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, "Courier New", Courier, monospace;
+      margin: 0;
+    }
+  }
+}
 </style>
