1e21d936551e286284f125f29b3c8268fb9f5aea
yaokai <yaokai729@outlook.com>
2022-03-25 16:58:25 +0800

    添加rollup配置文件描述

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 // 请先安装 rollup-plugin-esbuild rollup-plugin-vue rollup-plugin-scss sass rollup-plugin-terser
 // 为了保证版本一致，请复制我的 package.json 到你的项目，并把 name 改成你的库名
-import esbuild from 'rollup-plugin-esbuild'
+// rollup-plugin-esbuild 是用来把ts文件编译成js
+// rollup-plugin-vue  是用来把vue文件编译成js
+// rollup-plugin-scss  是用来把scss文件编译成js
+// rollup-plugin-terser 是用来简化(压缩)代码的
+import esbuild from 'rollup-plugin-esbuild'  
 import vue from 'rollup-plugin-vue'
 import scss from 'rollup-plugin-scss'
 import dartSass from 'sass';
