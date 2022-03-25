5ea32f89a133637a79cf39e0adf1f971261bfe61
yaokai <yaokai729@outlook.com>
2022-03-25 15:22:24 +0800

    添加rollup打包配置文件

---
+++ src/lib/index.ts
@@ -0,0 +1,9 @@
+// import Switch from './Switch.vue'
+// export { Switch }
+// 上面的两行可以简写为下面的一行
+export { default as Switch } from './Switch.vue';
+export { default as Button } from './Button.vue';
+export { default as Tabs } from './Tabs.vue';
+export { default as Tab } from './Tab.vue';
+export { default as Dialog } from './Dialog.vue';
+export { openDialog as openDialog } from './openDialog';
